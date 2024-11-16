71409c14222504d06f3c33a80e8d43ef034b428f
lucas.ferreira <lucas.bref82@gmail.com>
2024-11-16 18:48:41 -0300

    Constantes

---
+++ src/constant/Constantes.java
@@ -0,0 +1,11 @@
+package constant;
+
+public class Constantes {
+
+    private Constantes() {
+    }
+
+    public static final String DATA_SAIDA_MENOR_CHEGADA = "A data de chegada não pode ser maior que a data de saída.";
+    public static final String DATA_CHEGADA_MENOR_DATA_ATUAL = "A data de chegada não pode ser menor que a data atual.";
+    public static final String DATA_SAIDA_MENOR_DATA_ATUAL = "A data de saída não pode ser menor que a data atual.";
+}
